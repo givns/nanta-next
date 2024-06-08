ddb06fac4e5a11e87b8b6e4ced71a22f6472c650
givns <parunpat.p@gmail.com>
2024-06-09 01:02:44 +0700

    Add default homepage

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import OvertimeRequestList from '../components/OvertimeRequestList';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Nanta-Next</h1>
+      <OvertimeRequestList />
+    </div>
+  );
+};
+
+export default Home;
