da6df49a58d57701a6f2198b2cc1d20e05802321
givns <parunpat.p@gmail.com>
2024-06-18 11:55:06 +0700

    leaves form trigger

--- src/components/LeaveRequestForm.tsx
+++ src/components/LeaveRequestForm.tsx
@@ -156,10 +156,6 @@ const LeaveRequestForm = () => {
     }
   };
 
-  const formatThaiDate = (date: string) => {
-    return dayjs(date).locale('th').format('D MMM YYYY');
-  };
-
   return (
     <div className="main-container flex justify-center items-center h-screen">
       <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
