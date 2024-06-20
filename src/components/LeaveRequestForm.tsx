8d826097ddc728db924611ed92cc147d2caf0854
givns <parunpat.p@gmail.com>
2024-06-21 00:34:23 +0700

    leave request flow

--- src/components/LeaveRequestForm.tsx
+++ src/components/LeaveRequestForm.tsx
@@ -136,6 +136,10 @@ const LeaveRequestForm: React.FC = () => {
   return (
     <>
       <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, user-scalable=no"
+        />
         <style>
           {`
             input,
