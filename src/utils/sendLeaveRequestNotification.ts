274ed207d78054ae16b4a779f554edbe9ac3a3d3
givns <parunpat.p@gmail.com>
2024-06-22 07:32:25 +0700

    leave request flow

--- src/utils/sendLeaveRequestNotification.ts
+++ src/utils/sendLeaveRequestNotification.ts
@@ -11,17 +11,14 @@ const getLeaveCountForAdmin = async (adminId: string): Promise<number> => {
   let currentMonthStart: Date;
 
   if (now.getDate() < 26) {
-    // Before the 26th, get the 26th of the previous month
     const previousMonth = new Date(now.getFullYear(), now.getMonth() - 1, 26);
     currentMonthStart = previousMonth;
   } else {
-    // On or after the 26th, get the 26th of the current month
     currentMonthStart = new Date(now.getFullYear(), now.getMonth(), 26);
   }
 
   const leaveRequests = await prisma.leaveRequest.findMany({
     where: {
-      approverId: adminId,
       createdAt: {
         gte: currentMonthStart,
       },
