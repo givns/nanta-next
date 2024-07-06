76099bb066c63368d1c5fbfcc98fe8bd9778cf5d
givns <parunpat.p@gmail.com>
2024-07-06 11:14:39 +0700

    Adjust getlatest checkin

--- src/pages/api/runDiagnostics.ts
+++ src/pages/api/runDiagnostics.ts
@@ -13,11 +13,9 @@ export default async function handler(
   try {
     const externalDbService = new ExternalDbService();
     await externalDbService.runDiagnosticQueries();
-    res
-      .status(200)
-      .json({
-        message: 'Diagnostic queries completed. Check server logs for results.',
-      });
+    res.status(200).json({
+      message: 'Diagnostic queries completed. Check server logs for results.',
+    });
   } catch (error) {
     console.error('Error running diagnostic queries:', error);
     res.status(500).json({ message: 'Error running diagnostic queries' });
