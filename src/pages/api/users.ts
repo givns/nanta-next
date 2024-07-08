534f51289b1823d44eef4cea36791a07a2a76b4d
givns <parunpat.p@gmail.com>
2024-07-08 22:27:58 +0700

    Account for workday

--- src/pages/api/users.ts
+++ src/pages/api/users.ts
@@ -1,3 +1,5 @@
+// src/pages/api/users.ts
+
 import { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '../../lib/prisma';
 
@@ -5,17 +7,13 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  console.log('Received request to /api/users');
   const { lineUserId } = req.query;
 
   if (!lineUserId || typeof lineUserId !== 'string') {
-    console.log('Missing or invalid lineUserId:', lineUserId);
     return res
       .status(400)
       .json({ error: 'Missing or invalid lineUserId parameter' });
   }
-
-  console.log('Fetching user data for lineUserId:', lineUserId);
 
   try {
     const user = await prisma.user.findUnique({
@@ -26,24 +24,12 @@ export default async function handler(
     });
 
     if (!user) {
-      console.log('User not found for lineUserId:', lineUserId);
       return res.status(404).json({ error: 'User not found' });
-    }
-
-    console.log('User found:', user.id);
-
-    if (!user.employeeId) {
-      console.log('Employee ID not found for user:', user.id);
-      return res
-        .status(400)
-        .json({ error: 'Employee ID not found. Please contact support.' });
     }
 
     const today = new Date();
     const startOfMonth = new Date(today.getFullYear(), today.getMonth(), 1);
     const endOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0);
-
-    console.log('Fetching attendance data for user:', user.id);
 
     const recentAttendance = await prisma.attendance.findMany({
       where: {
@@ -74,7 +60,9 @@ export default async function handler(
           gte: startOfMonth,
           lte: endOfMonth,
         },
-        checkInTime: { not: null },
+        checkInTime: {
+          not: null,
+        },
       },
     });
 
@@ -91,9 +79,7 @@ export default async function handler(
       overtimeHours,
       balanceLeave,
     };
-    console.log('User data being sent:', JSON.stringify(responseData, null, 2));
 
-    console.log('Sending response for user:', user.id);
     res.status(200).json(responseData);
   } catch (error) {
     console.error('Error fetching user data:', error);
