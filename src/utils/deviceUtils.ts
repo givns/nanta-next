a997d6ec1a42841a86bdf8742f596f431b15ea55
givns <parunpat.p@gmail.com>
2024-07-25 23:31:53 +0700

    complete checkin funtion

--- src/utils/deviceUtils.ts
+++ src/utils/deviceUtils.ts
@@ -4,5 +4,5 @@ export const getDeviceType = (
   deviceSerial: string | null | undefined,
 ): string => {
   if (!deviceSerial) return 'ไม่ทราบ';
-  return deviceSerial === 'WEBAPP001' ? 'Nanta Next' : 'เครื่องสแกนใบหน้า';
+  return deviceSerial === 'WEBAPP001' ? 'Nanta next' : 'เครื่องสแกนใบหน้า';
 };
