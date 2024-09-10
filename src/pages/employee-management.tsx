0887489741d4497e2f2644b6c2a9072a8d0f854a
givns <parunpat.p@gmail.com>
2024-09-10 14:18:39 +0700

    Add new ImportUserProfilesForm component and other changes

---
+++ src/pages/employee-management.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import EmployeeManagement from '../components/EmployeeManagement';
+
+const EmployeeManagementPage: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center mt-10">Employee Management</h1>
+      <EmployeeManagement />
+    </div>
+  );
+};
+
+export default EmployeeManagementPage;
