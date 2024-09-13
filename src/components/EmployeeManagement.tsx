bab0adfcc4caf7ede6368830528837e820d96aad
givns <parunpat.p@gmail.com>
2024-09-13 08:57:31 +0700

    integrate import into employee management

--- src/components/EmployeeManagement.tsx
+++ src/components/EmployeeManagement.tsx
@@ -4,14 +4,7 @@ import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
 import liff from '@line/liff';
-import dynamic from 'next/dynamic'; // Import dynamic from next/dynamic
-
-const ImportUserProfilesForm = dynamic(
-  () => import('./ImportUserProfilesForm'),
-  {
-    ssr: false,
-  },
-);
+import ImportUserProfilesForm from './ImportUserProfilesForm';
 
 const EmployeeSchema = Yup.object().shape({
   name: Yup.string().required('Required'),
