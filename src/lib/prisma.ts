f709334d55e2e0799d2ade1752aa51ec8a5cefd2
givns <parunpat.p@gmail.com>
2024-10-07 12:25:56 +0700

    retract attendance and separate api user

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,17 +1,16 @@
 // lib/prisma.ts
-
 import { PrismaClient } from '@prisma/client';
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-const prisma =
-  global.prisma ||
+export const prisma =
+  globalForPrisma.prisma ??
   new PrismaClient({
     log: ['query', 'info', 'warn', 'error'],
   });
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 export default prisma;
