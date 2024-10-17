e11fee4aee30b52f88bae6fea14fcac29cdad8e2
givns <parunpat.p@gmail.com>
2024-10-17 16:49:40 +0700

    retract attendance and separate api user

--- src/components/CheckInOutForm.tsx
+++ src/components/CheckInOutForm.tsx
@@ -19,7 +19,13 @@ import LateReasonModal from './LateReasonModal';
 import ErrorBoundary from './ErrorBoundary';
 import ActionButton from './ActionButton';
 import { getCurrentTime, formatDate } from '../utils/dateUtils';
-import { addMinutes, isBefore, isSameDay, parseISO } from 'date-fns';
+import {
+  addMinutes,
+  isBefore,
+  isSameDay,
+  parseISO,
+  subMinutes,
+} from 'date-fns';
 
 interface CheckInOutFormProps {
   userData: UserData;
@@ -216,9 +222,9 @@ const CheckInOutForm: React.FC<CheckInOutFormProps> = ({
       }
     }
 
-    const EarlyCheckOut = isBefore(now, addMinutes(shiftEnd, 15)); // 15 minutes grace period
+    const EarlyCheckOut = subMinutes(shiftStart, 30); // 15 minutes grace period
 
-    if (EarlyCheckOut) {
+    if (!EarlyCheckOut) {
       const confirmed = window.confirm(
         'คุณกำลังจะลงเวลาออกก่อนเวลาเลิกงาน หากคุณต้องการลาป่วยฉุกเฉิน ระบบจะทำการยื่นคำขอลาป่วยเต็มวันให้อัตโนมัติ ต้องการดำเนินการต่อหรือไม่?',
       );
