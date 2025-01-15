b0a8072d94aa7aa35f856b12f2e54b43adf7fead
givns <parunpat.p@gmail.com>
2025-01-16 06:02:09 +0700

    new serialized attendance record

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -29,7 +29,7 @@ export function toBangkokTime(date: Date | string | number): Date {
 
 export function getCurrentTime(): Date {
   //For testing specific scenarios
-  const [datePart, timePart] = '2024-12-18T03:05'.split('T');
+  const [datePart, timePart] = '2024-12-18T04:20'.split('T');
   const time = timePart.split('+')[0];
   return parseISO(`${datePart}T${time}`);
 }
